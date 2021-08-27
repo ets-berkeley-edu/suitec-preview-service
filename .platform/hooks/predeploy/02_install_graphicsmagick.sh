5a3afedc1a9c15a357edc024e5194019406423cb
Pauline Kerschen <kerschen@berkeley.edu>
2021-08-27 14:14:10 -0700

    COL-1664 Adjustments to AL2 and Node configuration

---
+++ .platform/hooks/predeploy/02_install_graphicsmagick.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+amazon-linux-extras install epel -y
+yum --enablerepo=epel -y install GraphicsMagick
--- .platform/hooks/predeploy/05_install_local_configs.sh
+++ .platform/hooks/predeploy/05_install_local_configs.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-PYTHONPATH='' aws s3 cp s3://suitec-preview-service-config/${EB_ENVIRONMENT}.json config/local-production.json
+aws s3 cp s3://squiggy-deploy-configs/squiggy/${EB_ENVIRONMENT}.json config/local-production.json
