7a7cd15e4e9ea3bd73a6dbbca59a8c5163ae3f2b
Pauline Kerschen <kerschen@berkeley.edu>
2023-07-28 14:52:28 -0700

    COL-2208 Update versions on supplemental packages

--- .platform/hooks/predeploy/03_install_ffmpeg.sh
+++ .platform/hooks/predeploy/03_install_ffmpeg.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo mkdir -p /opt/ffmpeg
-sudo tar -xvf /tmp/ffmpeg-release-3.3.3-64bit-static.tar.xz -C /opt/ffmpeg
-sudo ln -sf /opt/ffmpeg/ffmpeg-3.3.3-64bit-static/ffmpeg /usr/local/bin/ffmpeg
-sudo ln -sf /opt/ffmpeg/ffmpeg-3.3.3-64bit-static/ffprobe /usr/local/bin/ffprobe
+sudo tar -xvf /tmp/ffmpeg-release-6.0-amd64-static.tar.xz -C /opt/ffmpeg
+sudo ln -sf /opt/ffmpeg/ffmpeg-6.0-amd64-static/ffmpeg /usr/local/bin/ffmpeg
+sudo ln -sf /opt/ffmpeg/ffmpeg-6.0-amd64-static/ffprobe /usr/local/bin/ffprobe
